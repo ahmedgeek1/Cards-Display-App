65d35941fcec6e66a3019120e9fea24d6ea03888
ahmedgeek1 <ahmedgeek1@gmail.com>
2017-11-22 14:59:21 -0600

    Cards App
    Adding the project's folder and all files

---
+++ Assessment2A/Assessment2A/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Assessment2A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn5hearts = new System.Windows.Forms.Button();
+            this.btn4spades = new System.Windows.Forms.Button();
+            this.btn3diamonds = new System.Windows.Forms.Button();
+            this.btn2clubs = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.welcometext = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic5hearts = new System.Windows.Forms.PictureBox();
+            this.pic4spades = new System.Windows.Forms.PictureBox();
+            this.pic3diamonds = new System.Windows.Forms.PictureBox();
+            this.pic2clubs = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5hearts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4spades)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3diamonds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2clubs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn5hearts
+            // 
+            this.btn5hearts.BackColor = System.Drawing.SystemColors.Control;
+            this.btn5hearts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5hearts.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5hearts.Location = new System.Drawing.Point(578, 202);
+            this.btn5hearts.Name = "btn5hearts";
+            this.btn5hearts.Size = new System.Drawing.Size(90, 29);
+            this.btn5hearts.TabIndex = 11;
+            this.btn5hearts.Text = "5 Hearts";
+            this.btn5hearts.UseVisualStyleBackColor = false;
+            this.btn5hearts.Click += new System.EventHandler(this.btn5hearts_Click);
+            // 
+            // btn4spades
+            // 
+            this.btn4spades.BackColor = System.Drawing.SystemColors.Control;
+            this.btn4spades.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4spades.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4spades.Location = new System.Drawing.Point(393, 202);
+            this.btn4spades.Name = "btn4spades";
+            this.btn4spades.Size = new System.Drawing.Size(90, 29);
+            this.btn4spades.TabIndex = 10;
+            this.btn4spades.Text = "4 Spades";
+            this.btn4spades.UseVisualStyleBackColor = false;
+            this.btn4spades.Click += new System.EventHandler(this.btn4spades_Click);
+            // 
+            // btn3diamonds
+            // 
+            this.btn3diamonds.BackColor = System.Drawing.SystemColors.Control;
+            this.btn3diamonds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3diamonds.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3diamonds.Location = new System.Drawing.Point(205, 202);
+            this.btn3diamonds.Name = "btn3diamonds";
+            this.btn3diamonds.Size = new System.Drawing.Size(90, 29);
+            this.btn3diamonds.TabIndex = 9;
+            this.btn3diamonds.Text = "3 Diamonds";
+            this.btn3diamonds.UseVisualStyleBackColor = false;
+            this.btn3diamonds.Click += new System.EventHandler(this.btn3diamonds_Click);
+            // 
+            // btn2clubs
+            // 
+            this.btn2clubs.BackColor = System.Drawing.SystemColors.Control;
+            this.btn2clubs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2clubs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2clubs.Location = new System.Drawing.Point(38, 202);
+            this.btn2clubs.Name = "btn2clubs";
+            this.btn2clubs.Size = new System.Drawing.Size(90, 29);
+            this.btn2clubs.TabIndex = 8;
+            this.btn2clubs.Text = "2 clubs";
+            this.btn2clubs.UseVisualStyleBackColor = false;
+            this.btn2clubs.Click += new System.EventHandler(this.btn2clubs_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.SystemColors.Control;
+            this.btnclose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnclose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.Color.Red;
+            this.btnclose.Location = new System.Drawing.Point(353, 258);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(90, 29);
+            this.btnclose.TabIndex = 16;
+            this.btnclose.Text = "close";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // btnreset
+            // 
+            this.btnreset.BackColor = System.Drawing.SystemColors.Control;
+            this.btnreset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnreset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreset.ForeColor = System.Drawing.Color.Green;
+            this.btnreset.Location = new System.Drawing.Point(241, 258);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(90, 29);
+            this.btnreset.TabIndex = 17;
+            this.btnreset.Text = "reset";
+            this.btnreset.UseVisualStyleBackColor = false;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
+            // 
+            // welcometext
+            // 
+            this.welcometext.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcometext.Location = new System.Drawing.Point(279, 1);
+            this.welcometext.Name = "welcometext";
+            this.welcometext.Size = new System.Drawing.Size(100, 32);
+            this.welcometext.TabIndex = 18;
+            this.welcometext.Text = "Welcome! ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 28);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Please click on the buttons to display cards";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pic5hearts
+            // 
+            this.pic5hearts.Image = global::Assessment2A.Properties.Resources._5_Hearts2;
+            this.pic5hearts.Location = new System.Drawing.Point(595, 82);
+            this.pic5hearts.Name = "pic5hearts";
+            this.pic5hearts.Size = new System.Drawing.Size(57, 81);
+            this.pic5hearts.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic5hearts.TabIndex = 15;
+            this.pic5hearts.TabStop = false;
+            this.pic5hearts.Visible = false;
+            // 
+            // pic4spades
+            // 
+            this.pic4spades.Image = global::Assessment2A.Properties.Resources._4_Spades2;
+            this.pic4spades.Location = new System.Drawing.Point(410, 82);
+            this.pic4spades.Name = "pic4spades";
+            this.pic4spades.Size = new System.Drawing.Size(57, 81);
+            this.pic4spades.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4spades.TabIndex = 14;
+            this.pic4spades.TabStop = false;
+            this.pic4spades.Visible = false;
+            // 
+            // pic3diamonds
+            // 
+            this.pic3diamonds.Image = global::Assessment2A.Properties.Resources._3_Diamonds2;
+            this.pic3diamonds.Location = new System.Drawing.Point(222, 82);
+            this.pic3diamonds.Name = "pic3diamonds";
+            this.pic3diamonds.Size = new System.Drawing.Size(57, 81);
+            this.pic3diamonds.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3diamonds.TabIndex = 13;
+            this.pic3diamonds.TabStop = false;
+            this.pic3diamonds.Visible = false;
+            // 
+            // pic2clubs
+            // 
+            this.pic2clubs.Image = global::Assessment2A.Properties.Resources._2_Clubs3;
+            this.pic2clubs.Location = new System.Drawing.Point(55, 82);
+            this.pic2clubs.Name = "pic2clubs";
+            this.pic2clubs.Size = new System.Drawing.Size(57, 81);
+            this.pic2clubs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2clubs.TabIndex = 12;
+            this.pic2clubs.TabStop = false;
+            this.pic2clubs.Visible = false;
+            this.pic2clubs.Click += new System.EventHandler(this.pic2clubs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(121)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(722, 312);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.welcometext);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.pic5hearts);
+            this.Controls.Add(this.pic4spades);
+            this.Controls.Add(this.pic3diamonds);
+            this.Controls.Add(this.btn5hearts);
+            this.Controls.Add(this.btn4spades);
+            this.Controls.Add(this.btn3diamonds);
+            this.Controls.Add(this.btn2clubs);
+            this.Controls.Add(this.pic2clubs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic5hearts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4spades)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3diamonds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2clubs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic5hearts;
+        private System.Windows.Forms.PictureBox pic4spades;
+        private System.Windows.Forms.PictureBox pic3diamonds;
+        private System.Windows.Forms.PictureBox pic2clubs;
+        private System.Windows.Forms.Button btn5hearts;
+        private System.Windows.Forms.Button btn4spades;
+        private System.Windows.Forms.Button btn3diamonds;
+        private System.Windows.Forms.Button btn2clubs;
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.Button btnreset;
+        private System.Windows.Forms.Label welcometext;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assessment2A/Assessment2A/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assessment2A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //displaying the card 2Clubs
+        private void btn2clubs_Click(object sender, EventArgs e)
+        {
+            pic2clubs.Visible = true;
+        }
+
+        //displaying the card 3Diamonds
+        private void btn3diamonds_Click(object sender, EventArgs e)
+        {
+            pic3diamonds.Visible = true;
+        }
+
+        //displaying the card 4Spades
+        private void btn4spades_Click(object sender, EventArgs e)
+        {
+            pic4spades.Visible = true;
+        }
+
+        //displaying the card 5Hearts
+        private void btn5hearts_Click(object sender, EventArgs e)
+        {
+            pic5hearts.Visible = true;
+        }
+
+        //hide all cards
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            pic2clubs.Visible = false;
+            pic3diamonds.Visible = false;
+            pic4spades.Visible = false;
+            pic5hearts.Visible = false;
+        }
+
+        //close the application
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pic2clubs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
